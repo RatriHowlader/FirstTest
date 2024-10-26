c48446b244e61364ae44486bb36a015cf1af3d03
Student <Student@LAB513-PC15>
2024-10-26 13:18:43 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AventStack.ExtentReports;
+using CsvHelper;
+using FirstTest;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Formats.Asn1;
+using System.Globalization;
+
+public class FirstSelenium
+{
+    public static string DataCSVFile = System.IO.Directory.GetCurrentDirectory();
+    private static TextReader reader;
+
+    static void Main(string[] args)
+    {
+        var testDataList = ReadCsvData(DataCSVFile + "\\Data\\Data.csv");
+        IWebDriver driver = new ChromeDriver();
+        ExtentReports extentReports = new ExtentReports();
+
+
+        driver.Navigate().GoToUrl("https://practicetestautomation.com/practice-test-login/");
+        Console.WriteLine("Open Browser");
+
+        driver.Manage().Window.Maximize();
+        Console.WriteLine("Browser Maximize");
+        foreach (var test in testDataList)
+        {
+            driver.FindElement(By.Id("username")).SendKeys("student");
+            Console.WriteLine("Provide username");
+
+            driver.FindElement(By.Id("password")).SendKeys("Password123");
+            Console.WriteLine("Provide Password");
+
+            driver.FindElement(By.Id("submit")).Click();
+            Console.WriteLine("Hit Submit button");
+        }
+
+      
+        try
+        {
+            driver.FindElement(By.CssSelector(".wp-block-button__link")).Click();
+        }
+        catch
+        {
+            Console.WriteLine("Failed Login");
+        }
+
+
+
+    }
+    static List<Testdata> ReadCsvData(String filePath)
+    {
+        using (var reader = new StreamReader(filePath))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            return new List<Testdata>(csv.GetRecords<Testdata>());
+        }
+    }
+}
---
+++ Testdata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTest
+{
+    internal class Testdata
+    {
+        public string username {  get; set; }
+        public string password { get; set; }
+    }
+}
